c1cb4dbc3f13eb28b8f5814bc533937c4f5c802f
Łukasz Czyż <krawacik3@gmail.com>
2018-06-30 15:00:26 +0200

    modified Neuralnet class

--- inc/neuralnet.hpp
+++ inc/neuralnet.hpp
@@ -10,18 +10,21 @@ class Perceptron {
 template< typename type >
 class Neuralnet {
 
-    vector < type > input;
-    vector < type > output;
-    vector < Perceptron> hiddenLayer;
+    vector <type> input;
+    vector <type> output;
+    vector <Perceptron> hiddenLayer;
 
+    int *ih_weight;
 
     Neuralnet ( unsigned int inputCount = 2, 
                 unsigned int hiddenLayerCount = 3,
                 unsigned int outputCount = 2 ) {
 
-                    input.resize ( inputCount );
+                    input.resize ( inputCount + 1 );        //Vector holding all input values and bias = 1
                     input.resize ( outputCount );
                     hiddenLayer.resize ( hiddenLayerCount );
+
+                    ih_weight = new Matrix <type> ( 1, inputCount + 1 );
                 }
 };
 
