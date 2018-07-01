d44313985c72b74a970647afec41c8a3d36b51b7
Zwo1in <zwolin13@gmail.com>
2018-07-01 15:16:12 +0200

    Rebuilding...

--- inc/neuralnet.hpp
+++ inc/neuralnet.hpp
@@ -1,36 +1,20 @@
 #ifndef NEURALNET_HPP_
 #define NEURALNET_HPP_
 
-using namespace std;
+#include "matrix.hpp"
 
-class Perceptron {
-    float bias;
-};
-
-template< typename type >
 class Neuralnet {
+    int hLayers;
+
+    std::vector< Matrix<float> > hValues;
+    std::vector< Matrix<float> > hWages;
+
     public:
-    vector <type> input;
-    vector <type> output;
-
-
-    Matrix <type> *weights;
-
-    Neuralnet ( unsigned int inputCount = 2, 
-                unsigned int outputCount = 2 ) {
-
-                    input.resize ( inputCount + 1 );        //Vector holding all input values and bias = 1
-                    input.resize ( outputCount );
-
-                    weights = new Matrix <type> ( inputCount + 1, outputCount );
-                    weights->randomize ( 1, -1 );
-
-                    // for ( int i = 0; i < outputCount; ++i ) {
-                    //     weights[]
-                    // }
-
-                    cout << *weights;
-                }
+    Neuralnet( int inputsNum, int hiddenNum, int outputsNum ) {
+        hValues.emplace_back( hiddenNum, 1 );
+        hWages.emplace_back( inputsNum, hiddenNum );
+        hWages.emplace_back( hiddenNum, outputsNum );
+    }
 };
 
 #endif
