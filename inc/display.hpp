667f4f43c2a2a55f6a4632343401f28b476b31c4
Maciej Zwoliński <zwolin13@gmail.com>
2018-07-03 16:17:56 +0200

    ...

--- inc/display.hpp
+++ inc/display.hpp
@@ -1,7 +1,18 @@
 
+bool handleEvents( sf::RenderWindow& win, int& count ) {
+    sf::Event event;
+    while ( win.pollEvent( event ) ) {	
+        if ( event.type == sf::Event::Closed ) win.close();	
+        else if ( event.type == sf::Event::KeyPressed ) {
+            switch (event.key.code) {
+                case sf::Keyboard::Escape:      win.close(); return true;
+                case sf::Keyboard::Space:       ++count; break;
+            }
+        }	
+    }
+}
 
-
-void drawImage ( sf::RenderWindow *win, Matrix <float> img ) {
+void drawImage ( sf::RenderWindow &win, Matrix <float> img ) {
     sf::RectangleShape pixel;
     pixel.setSize( sf::Vector2f(10, 10) );
 
@@ -10,12 +21,12 @@ void drawImage ( sf::RenderWindow *win, Matrix <float> img ) {
 
             pixel.setFillColor ( sf::Color( img[j][i], img[j][i], img[j][i] ) );
             pixel.setPosition( 10 + 10 * i, 10 + 10 * j );
-            win->draw( pixel );
+            win.draw( pixel );
         }
     }
 }
 
-void drawText ( sf::RenderWindow *win, int pos_x, int pos_y, string displayText ) {
+void drawText ( sf::RenderWindow &win, int pos_x, int pos_y, string displayText ) {
     sf::Font font;
     font.loadFromFile( "theboldfont.ttf" );
 
@@ -24,16 +35,16 @@ void drawText ( sf::RenderWindow *win, int pos_x, int pos_y, string displayText 
     text.setString( displayText );
     text.setPosition ( pos_x, pos_y );
 
-    win->draw( text );
+    win.draw( text );
 }
 
-void drawOutput ( sf::RenderWindow *win, int pos_x, int pos_y, int size,  Matrix <float> output ) {
+void drawOutput ( sf::RenderWindow &win, int pos_x, int pos_y, int size,  Matrix <float> output ) {
     sf::CircleShape circle;
     circle.setRadius( size );
     for (int i = 0; i < output.getSize().second; ++i ) {
         circle.setFillColor ( sf::Color( 255 * output[0][i], 255 * output[0][i], 255 * output[0][i] ) );
         circle.setPosition ( pos_x, pos_y + i * 3 * size );
-        win->draw( circle );
+        win.draw( circle );
         drawText( win, pos_x + 3 * size, pos_y + i * 3 * size, to_string( output[0][i] ) );
         drawText( win, pos_x - 3 * size, pos_y + i * 3 * size, to_string( i ) + " -" );
     }
--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,45 @@
 
 using namespace std;
 int main() {
+    sf::RenderWindow win ( sf::VideoMode( 800, 800 ), "Neural Net");
+
+    srand( time( NULL ) );
+    Idx idx;
+    int count{};
+    
+    NeuralNet nn( 784, {12, 12}, 10 );
+
     try {
+        while( win.isOpen() ) {
+            // Process window events
+            if ( handleEvents( win, count ) )   
+                break;
 
-    srand( time( NULL ) ); /*
-    fpsClock clock(1);
-    sf::RenderWindow win ( sf::VideoMode( width, height ), "Neural Net");
+            Matrix <float> img = idx.getImage(count);
+            Matrix <float> lbl = idx.getLabel(count);
+            int digit = int( lbl[0][0] );
+            Matrix <float> exp_output(1, 10);
 
-    NeuralNet nn( 2, {10,5,10}, 2 );
+            exp_output.fill( 0 );
+            exp_output[0][digit] = 1;
+            if(count <= 1000) {
+                ++count;
+                nn.backpropagate( reshapeMatrix( img ).T(), exp_output.T() );
+            }
+
+            win.clear( sf::Color( 51, 51, 51 ) );
+            drawImage( win, img );
+            drawText( win, 10 , 300, "Current digit: " + to_string( digit ) );
+            drawText( win, 10, 350, "Count: " + to_string( count ) );
+            drawOutput( win, 500, 10, 20, nn.feedforward( reshapeMatrix( img ).T() ).T() );
+        
+            win.display();
+        }
+    } catch (const char* error) {
+        std::cout << error << std::endl;
+    }
+    return 0;
+}
 
 // XOR -------------------------------------------------------------------------
     // std::vector<Matrix<float>> trainSet;
@@ -47,98 +79,54 @@ int main() {
 
 // GRAPHICAL TEST --------------------------------------------------------------
 // SET UP GRAPHICAL AREA IN POINT LINE 50
-        std::vector< TrainP > drawSet;
-        for ( int i = 0; i < 2000; ++i )
-            drawSet.emplace_back();
+        // std::vector< TrainP > drawSet;
+        // for ( int i = 0; i < 2000; ++i )
+        //     drawSet.emplace_back();
 
-        // Draw the expected -------------------------------------
-        win.clear( sf::Color(51, 51, 51) );
-        for ( auto& point : drawSet )
-            point.draw(win);
-        win.display();
-        // -------------------------------------------------------
-        // Pretraining -------------------------------------------
-        for ( int j = 0; j < 50; ++j ) {
-            for ( int i = 0; i < 10000; ++i ) {
-                TrainP trainP;
-                nn.backpropagate( trainP.coords, trainP.isAbove );
-            }
-            std::cout << j << std::endl;
-        }
-        // -------------------------------------------------------
-        while ( win.isOpen() ) {
-            if ( clock.tick() ) {
-                sf::Event event;
-                while ( win.pollEvent( event ) ) {
-                    if( event.type == sf::Event::Closed )
-                        win.close();
-                }
+        // // Draw the expected -------------------------------------
+        // win.clear( sf::Color(51, 51, 51) );
+        // for ( auto& point : drawSet )
+        //     point.draw(win);
+        // win.display();
+        // // -------------------------------------------------------
+        // // Pretraining -------------------------------------------
+        // for ( int j = 0; j < 50; ++j ) {
+        //     for ( int i = 0; i < 10000; ++i ) {
+        //         TrainP trainP;
+        //         nn.backpropagate( trainP.coords, trainP.isAbove );
+        //     }
+        //     std::cout << j << std::endl;
+        // }
+        // // -------------------------------------------------------
+        // while ( win.isOpen() ) {
+        //     if ( clock.tick() ) {
+        //         sf::Event event;
+        //         while ( win.pollEvent( event ) ) {
+        //             if( event.type == sf::Event::Closed )
+        //                 win.close();
+        //         }
 
-                // Training step -------------------------------------
-                for ( int i = 0; i < 10; ++i ) {
-                    TrainP trainP;
-                    nn.backpropagate( trainP.coords, trainP.isAbove );
-                }
-                // ---------------------------------------------------
-                for ( auto& point : drawSet ) {
-                    auto res = nn.feedforward( point.coords );
-                    if ( res[0][0] && res[1][0] )
-                        point.color();
-                    else if ( res[0][0] && !res[1][0] )
-                        point.color( 2 );
-                    else
-                        point.color(1);
-                }
+        //         // Training step -------------------------------------
+        //         for ( int i = 0; i < 10; ++i ) {
+        //             TrainP trainP;
+        //             nn.backpropagate( trainP.coords, trainP.isAbove );
+        //         }
+        //         // ---------------------------------------------------
+        //         for ( auto& point : drawSet ) {
+        //             auto res = nn.feedforward( point.coords );
+        //             if ( res[0][0] && res[1][0] )
+        //                 point.color();
+        //             else if ( res[0][0] && !res[1][0] )
+        //                 point.color( 2 );
+        //             else
+        //                 point.color(1);
+        //         }
 
-                win.clear( sf::Color(51, 51, 51) );
-                for ( auto& point : drawSet )
-                    point.draw(win);
-                win.display();
-            }
-        }
-        return 0;
+        //         win.clear( sf::Color(51, 51, 51) );
+        //         for ( auto& point : drawSet )
+        //             point.draw(win);
+        //         win.display();
+        //     }
+        // }
+        // return 0;
 //------------------------------------------------------------------------------
-    } catch ( const char* message ) {
-        std::cout << message << std::endl; */
-    sf::RenderWindow win ( sf::VideoMode( 800, 800 ), "Neural Net");
-    Idx idx;
-    int count{};
-    
-    NeuralNet nn( 784, {12, 12}, 10 );
-
-    while( win.isOpen() ) {
-
-        // Process window events
-        sf::Event event;
-        while ( win.pollEvent( event ) ) {	
-            if ( event.type == sf::Event::Closed ) win.close();	
-            if ( event.type == sf::Event::KeyPressed ) {
-                if ( event.key.code == sf::Keyboard::Escape ) win.close();	
-                if ( event.key.code == sf::Keyboard::Space ) ++count;
-            }	
-        }
-
-        Matrix <float> img = idx.getImage(count);
-        Matrix <float> lbl = idx.getLabel(count);
-        int digit = int( lbl[0][0] );
-        Matrix <float> exp_output(1, 10);
-
-        exp_output.fill( 0 );
-        exp_output[0][digit] = 1;
-        if(count <= 1000) {
-            ++count;
-            nn.train( reshapeMatrix( img ).T(), exp_output.T() );
-        }
-
-        win.clear( sf::Color( 69, 69, 69 ) );
-        drawImage( &win, img );
-        drawText( &win, 10 , 300, "Current digit: " + to_string( digit ) );
-        drawText( &win, 10, 350, "Count: " + to_string( count ) );
-        drawOutput( &win, 500, 10, 20, nn.feedforward( reshapeMatrix( img ).T() ).T() );
-       
-        win.display();
-    }
-
-
-    return 0;
-}
